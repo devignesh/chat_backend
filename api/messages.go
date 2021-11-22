f7fd47f87650d3c272402cc2cbd47bab56078819
devignesh <vinesh1865@gmail.com>
2021-11-23 01:11:00 +0530

    message api pkg

---
+++ api/messages.go
@@ -0,0 +1,97 @@
+package api
+
+import (
+	"chat_backend/schema"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func CreateMessage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var message schema.Messages
+	err = json.NewDecoder(r.Body).Decode(&message)
+	if err != nil {
+		log.Fatalf("Unable to decode the request body.  %v", err)
+	}
+
+	if err = db.Create(&message).Error; err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(&message)
+	json.NewEncoder(w).Encode(&message)
+
+}
+
+func GetMessages(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var message []schema.Messages
+	err := db.Find(&message)
+	if err == nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("{}", message)
+	json.NewEncoder(w).Encode(&message)
+
+}
+
+func GetMessage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var message schema.Messages
+	err := db.First(&message, params["id"])
+	if err == nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(&message)
+}
+
+func UpdateMessage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var message schema.Messages
+	err := json.NewDecoder(r.Body).Decode(&message)
+
+	if err != nil {
+		log.Fatalf("Unable to decode the request body.  %v", err)
+	}
+
+	if err = db.Debug().Model(&schema.Messages{}).Where("id = ?", params["id"]).Take(&schema.Messages{}).UpdateColumns(
+		map[string]interface{}{
+			"name":       message.Content,
+			"updated_at": time.Now(),
+		},
+	).Error; err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("updated successfully")
+
+	db.First(&message, params["id"])
+	json.NewEncoder(w).Encode(&message)
+
+}
+
+func DeleteMessage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var message schema.Messages
+	if err = db.First(&message, params["id"]).Error; err != nil {
+		log.Fatal(err)
+
+	}
+
+	err := db.Delete(&message)
+	if err == nil {
+		log.Println("successfully deleted")
+	}
+
+	json.NewEncoder(w).Encode(&message)
+}
