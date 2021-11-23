2b29d57341526487fd9131a51a8743f6b226b9f5
devignesh <vinesh1865@gmail.com>
2021-11-23 09:14:07 +0530

    adding comments to the api functions

--- api/chatrooms.go
+++ api/chatrooms.go
@@ -18,6 +18,7 @@ import (
 var db *gorm.DB
 var err error
 
+//database connection using gorm.io/gorm
 func DbConn() {
 	dsn := "host=localhost port=5432 user=postgres dbname=chatbackend sslmode=disable password=postgres"
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -28,6 +29,7 @@ func DbConn() {
 
 	log.Println("db connected successfully")
 
+	//Automigrate use to migrate the go tables
 	err := db.AutoMigrate(&schema.Chatroom{}, &schema.Users{}, &schema.Messages{})
 	if err != nil {
 		log.Fatal(err)
@@ -35,8 +37,11 @@ func DbConn() {
 	// defer db.Close()
 }
 
+//create a new chatroom, insert the new record in the chatroom table
 func CreateChatroom(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
+	//initialize the chatroom req types
 	var req types.ChatroomReq
 	err = json.NewDecoder(r.Body).Decode(&req)
 
@@ -45,35 +50,42 @@ func CreateChatroom(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var chatroom schema.Chatroom
+	//assign the roomname
 	chatroom.RoomName = req.RoomName
 	if err = db.Create(&chatroom).Error; err != nil {
 		log.Fatal(err)
 	}
 
 	log.Println(&chatroom)
+	//initialize the empty chatroomuser list for join table
 	var chatroomusers []schema.ChatroomUsers
 
+	//load the all userid and append the chatroomid and userid into chatroomusers table
 	for _, v := range req.UserIDs {
 		chatroomusers = append(chatroomusers, schema.ChatroomUsers{ChatroomID: chatroom.ID, UsersID: v})
 	}
 
 	fmt.Println(chatroomusers)
 
+	//create the new record
 	if err = db.Create(&chatroomusers).Error; err != nil {
 		log.Fatal(err)
 	}
 
 	var chatrooms schema.Chatroom
+	//select the chatroom records based on the chatroomid and it preloads the users information from chatroomusrs
 	db.Preload("Users").First(&chatrooms, chatroom.ID)
 
 	json.NewEncoder(w).Encode(&chatrooms)
 
 }
 
+//get the list of chatrooms in the table
 func GetChatrooms(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var chatrooms []schema.Chatroom
 
+	//find all the records from table with preload the users records
 	if err = db.Preload("Users").Find(&chatrooms).Error; err != nil {
 		log.Fatal(err)
 	}
@@ -84,13 +96,14 @@ func GetChatrooms(w http.ResponseWriter, r *http.Request) {
 
 }
 
+//get the single record of chatroom based on the id
 func GetChatroom(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	params := mux.Vars(r)
-	// fmt.Println("test vale", params)
 	var chatroom schema.Chatroom
 
+	//fetch the matching record based on the request id with preloads the usrs information
 	if err = db.Preload("Users").First(&chatroom, params["id"]).Error; err != nil {
 		log.Fatal(err)
 	}
@@ -101,6 +114,8 @@ func GetChatroom(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
+//update the chatroom record based on the id
 func UpdateChatroom(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -111,6 +126,7 @@ func UpdateChatroom(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("Unable to decode the request body.  %v", err)
 	}
 
+	//update the record based on the id and Take return a record that match given conditions
 	if err = db.Debug().Model(&schema.Chatroom{}).Where("id = ?", params["id"]).Take(&schema.Chatroom{}).UpdateColumns(
 		map[string]interface{}{
 			"room_name":  chatroom.RoomName,
@@ -122,6 +138,7 @@ func UpdateChatroom(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("updated successfully")
 
+	//after updation it gives the response along with users record
 	if err = db.Preload("Users").First(&chatroom, params["id"]).Error; err != nil {
 		log.Println(err)
 	}
@@ -130,6 +147,7 @@ func UpdateChatroom(w http.ResponseWriter, r *http.Request) {
 
 }
 
+//delete chatroom function
 func DeleteChatroom(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
